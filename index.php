fa723a86a5f1d0799f7f610973f9087505e59756
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-05-15 11:36:23 +0300

    remove punctuation

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // Program input
 $blacklistFilename = 'global.fsd.xml'; // Blacklist XML file from http://www.basistech.com/text-analytics/rosette/name-indexer/#
 $noisewordsFilename = 'noise.txt';
-$namesToCheck = array("gabriel robert mugabe", "saddam al-tikriti hussein");
+$namesToCheck = array("Gabriel robert mr Mugabe", "saddam AL-tikriti hussein");
 
 // Program run order
 $noisewords = importNoiseWords($noisewordsFilename);
@@ -50,8 +50,12 @@ function sanitizeName($name) {
 	// Force lowercase
 	$name = strtolower($name);
 
+	// Remove punctuation
+	// $name = preg_replace('/[^\pL\s]+/i', '-', $name); // better, but seems to breac unicode letters
+	$name = preg_replace('/[\.,\'#@\s]+/i', ' ', $name);
+
 	////// Array manipulation ////////
-	$nameArray = explode(" ", $name);
+	$nameArray = preg_split('/\s+/', $name); // also handle multiple spaces
 
 	// Remove noise words
 	$nameArray = array_diff($nameArray, $noisewords);
@@ -70,8 +74,8 @@ function printMatches($list) {
 
 	print '<pre>';
 	foreach ($list as $name) {
-		$name = sanitizeName($name);
-		if (in_array($name, $blacklist)) {
+		$sanitized = sanitizeName($name);
+		if (in_array($sanitized, $blacklist)) {
 			print_r("$name\n");
 		}
 	}
